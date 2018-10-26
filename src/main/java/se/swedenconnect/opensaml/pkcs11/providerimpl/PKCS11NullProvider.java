07e873d64b4c41a2cda888289d21b2f21a4ee6b1
Martin Lindström <martin.lindstrom@litsec.se>
2018-10-26 16:00:46 +0200

    Fixed copyright statement

--- src/main/java/se/swedenconnect/opensaml/pkcs11/providerimpl/PKCS11NullProvider.java
+++ src/main/java/se/swedenconnect/opensaml/pkcs11/providerimpl/PKCS11NullProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Swedish Agency for Digital Government
+ * Copyright 2018 Sweden Connect
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
