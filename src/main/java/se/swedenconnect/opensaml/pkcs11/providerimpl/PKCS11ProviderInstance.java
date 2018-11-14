2c3a72175d9afc1687a893f1a5e8db73094ec014
Stefan Santesson <stefan@aaa-sec.com>
2018-11-15 00:38:02 +0100

    Javadoc and license fix

--- src/main/java/se/swedenconnect/opensaml/pkcs11/providerimpl/PKCS11ProviderInstance.java
+++ src/main/java/se/swedenconnect/opensaml/pkcs11/providerimpl/PKCS11ProviderInstance.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Sweden Connect
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package se.swedenconnect.opensaml.pkcs11.providerimpl;
 
 
@@ -10,12 +25,11 @@ import java.security.Provider;
  * <pre>{@code
  *  // Typical Java 8 implementation.
  *  Provider p11provider = new SunPKCS11(configInputStream);
- **
+ *
  *  // Typical java 9+ implementation
  *  // configData is either a file name or "--" + configDataString
  *  Provider p = Security.getProvider("SunPKCS11");
- *    p = p.configure(configData);
- *    Security.addProvider(p);}</pre>
+ *    p = p.configure(configData);}</pre>
  *
  */
 public interface PKCS11ProviderInstance {
